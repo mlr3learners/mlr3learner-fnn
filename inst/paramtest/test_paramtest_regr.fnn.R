0733063371c29dba6ada92e9249c758ebb098ae3
pat-s <patrick.schratz@gmail.com>
2020-04-01 20:54:33 +0200

    lintr

--- inst/paramtest/test_paramtest_regr.fnn.R
+++ inst/paramtest/test_paramtest_regr.fnn.R
@@ -4,15 +4,16 @@ test_that("regr.fnn", {
   learner = lrn("regr.fnn")
   fun = FNN::knn.reg # replace!
   exclude = c(
-  "train", # handled by mlr
-  "test", # handled by mlr
-  "y" # handled by mlr
+    "train", # handled by mlr
+    "test", # handled by mlr
+    "y" # handled by mlr
   )
 
   ParamTest = run_paramtest(learner, fun, exclude)
-  expect_true(ParamTest, info = paste0("
+  expect_true(ParamTest, info = paste0(
+    "
 Missing parameters:
 ",
-    paste0("- '", ParamTest$missing,"'", collapse = "
+    paste0("- '", ParamTest$missing, "'", collapse = "
 ")))
 })
